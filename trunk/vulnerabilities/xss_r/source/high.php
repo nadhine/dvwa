45dc3a9aa1e54e4c8051eefa33fe24ccec404237
vraffael.lins@gmail.com <vraffael.lins@gmail.com@efff4f47-75f7-380c-dc63-677f0af0d85b>
2014-05-25 17:43:34 +0000

    Vulnerability: Reflected Cross Site Scripting (XSS) DVWA Security: High

--- trunk/vulnerabilities/xss_r/source/high.php
+++ trunk/vulnerabilities/xss_r/source/high.php
@@ -1,15 +1,17 @@
 <?php
 	
 if(!array_key_exists ("name", $_GET) || $_GET['name'] == NULL || $_GET['name'] == ''){
+
+	$isempty = true;
 	
- $isempty = true;
-		
 } else {
-	
- $html .= '<pre>';
- $html .= 'Hello ' . htmlspecialchars($_GET['name']);
- $html .= '</pre>';
-		
+	/* Previnir ataques XSS com codificação UTF-7 */
+
+	$name = htmlentities($_GET['name'], ENT_QUOTES);
+
+	$html .= '<pre>';
+	$html .= 'Hello ' . $name;
+	$html .= '</pre>';
 }
 
 ?>

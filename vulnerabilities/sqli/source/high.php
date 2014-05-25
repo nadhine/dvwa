507aeaa2e5d772faa4823173e1937262eceb0b6a
vraffael.lins@gmail.com <vraffael.lins@gmail.com@efff4f47-75f7-380c-dc63-677f0af0d85b>
2014-05-25 13:54:54 +0000

    Vulnerability: SQL Injection DVWA Security: High

--- vulnerabilities/sqli/source/high.php
+++ vulnerabilities/sqli/source/high.php
@@ -10,6 +10,8 @@ if (isset($_GET['Submit'])) {
 
 	if (is_numeric($id)){
 
+		$id = (int)$id;
+
 		$getid = "SELECT first_name, last_name FROM users WHERE user_id = '$id'";
 		$result = mysql_query($getid) or die('<pre>' . mysql_error() . '</pre>' );
 
@@ -17,11 +19,10 @@ if (isset($_GET['Submit'])) {
 
 		$i=0;
 
-		while ($i < $num) {
-
+		if ($num == 1) {
 			$first = mysql_result($result,$i,"first_name");
 			$last = mysql_result($result,$i,"last_name");
-			
+
 			$html .= '<pre>';
 			$html .= 'ID: ' . $id . '<br>First name: ' . $first . '<br>Surname: ' . $last;
 			$html .= '</pre>';

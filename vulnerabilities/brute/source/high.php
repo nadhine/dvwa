d4ff5890d4b3755c03bfd78d8e6fe1c0dc307567
vraffael.lins@gmail.com <vraffael.lins@gmail.com@efff4f47-75f7-380c-dc63-677f0af0d85b>
2014-05-25 03:05:43 +0000

    Vulnerability: Brute Force DVWA Security: High

--- vulnerabilities/brute/source/high.php
+++ vulnerabilities/brute/source/high.php
@@ -13,24 +13,59 @@ if( isset( $_GET[ 'Login' ] ) ) {
 	$pass = mysql_real_escape_string( $pass );
 	$pass = md5( $pass );
 
-	$qry = "SELECT * FROM `users` WHERE user='$user' AND password='$pass';";
-	$result = mysql_query($qry) or die('<pre>' . mysql_error() . '</pre>' );
-
-	if( $result && mysql_num_rows( $result ) == 1 ) {
-		// Get users details
-		$i=0; // Bug fix.
-		$avatar = mysql_result( $result, $i, "avatar" );
-
-		// Login Successful
-		$html .= "<p>Welcome to the password protected area " . $user . "</p>";
-		$html .= '<img src="' . $avatar . '" />';
-	} else {
-		// Login failed
+	if (blocked_user($user)) {
 		sleep(3);
-		$html .= "<pre><br>Username and/or password incorrect.</pre>";
+		$html .= "<pre><br>Block user on 5 unsuccessful login attempts.</pre>";
+	} else {
+		$qry = "SELECT * FROM `users` WHERE user='$user' AND password='$pass';";
+		$result = mysql_query($qry) or die('<pre>' . mysql_error() . '</pre>' );
+
+		if( $result && mysql_num_rows( $result ) == 1 ) {
+			// Get users details
+			$i=0; // Bug fix.
+			$avatar = mysql_result( $result, $i, "avatar" );
+			
+			// Login Successful
+			reset_login_attempts($user);
+
+			$html .= "<p>Welcome to the password protected area " . $user . "</p>";
+			$html .= '<img src="' . $avatar . '" />';
+		} else {
+			// Login failed
+			increase_login_attempts($user);
+
+			sleep(3);
+			$html .= "<pre><br>Username and/or password incorrect.</pre>";
 		}
+	}
 
 	mysql_close();
+}
+
+function reset_login_attempts($user){
+	$update = "UPDATE `users` set login_attempts='0' WHERE user='$user';";
+	mysql_query($update) or die('<pre>' . mysql_error() . '</pre>' );
+}
+
+function increase_login_attempts($user){
+	$update = "UPDATE `users` set login_attempts=login_attempts+1 WHERE user='$user';";
+	mysql_query($update) or die('<pre>' . mysql_error() . '</pre>' );
+}
+
+function blocked_user($user){
+	$max_login_attempts = 5;
+
+	$qry = "SELECT * FROM `users` WHERE user='$user';";
+	$result = mysql_query($qry) or die('<pre>' . mysql_error() . '</pre>' );
+
+	if ($result && mysql_num_rows($result) == 1) {
+		$i=0;
+		$login_attempts = mysql_result($result, $i, "login_attempts");
+
+		return $login_attempts >= $max_login_attempts; 
+	}
+
+	return false;
 }
 
 ?>

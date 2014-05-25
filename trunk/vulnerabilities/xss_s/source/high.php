eb7bb3220ef56bcebd074f18e27a59dc36c3d355
vraffael.lins@gmail.com <vraffael.lins@gmail.com@efff4f47-75f7-380c-dc63-677f0af0d85b>
2014-05-25 18:07:59 +0000

    Vulnerability: Stored Cross Site Scripting (XSS) DVWA Security: High

--- trunk/vulnerabilities/xss_s/source/high.php
+++ trunk/vulnerabilities/xss_s/source/high.php
@@ -1,24 +1,25 @@
 <?php
 
-if(isset($_POST['btnSign']))
-{
+if(isset($_POST['btnSign'])) {
 
-   $message = trim($_POST['mtxMessage']);
-   $name    = trim($_POST['txtName']);
-   
-   // Sanitize message input
-   $message = stripslashes($message);
-   $message = mysql_real_escape_string($message);
-   $message = htmlspecialchars($message);
-   
-   // Sanitize name input
-   $name = stripslashes($name);
-   $name = mysql_real_escape_string($name); 
-   $name = htmlspecialchars($name);
-  
-   $query = "INSERT INTO guestbook (comment,name) VALUES ('$message','$name');";
-   
-   $result = mysql_query($query) or die('<pre>' . mysql_error() . '</pre>' );
+	$message = trim($_POST['mtxMessage']);
+	$name    = trim($_POST['txtName']);
+
+	// Sanitize message input
+	$message = stripslashes($message);
+	$message = mysql_real_escape_string($message);
+	$message = htmlspecialchars($message);
+
+	// Sanitize name input
+	$name = stripslashes($name);
+	$name = mysql_real_escape_string($name);
+
+	/* Previnir ataques XSS com codificação UTF-7 */
+	$name = htmlentities($name, ENT_QUOTES);
+
+	$query = "INSERT INTO guestbook (comment,name) VALUES ('$message','$name');";
+
+	$result = mysql_query($query) or die('<pre>' . mysql_error() . '</pre>' );
    
 }
 
